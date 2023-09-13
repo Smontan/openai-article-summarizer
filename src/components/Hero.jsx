41d307cb622b8b9771a4b49333134768376ba79e
Sherwin <94516340+Smontan@users.noreply.github.com>
2023-09-13 21:25:29 +0900

    h2 edited at the hero.jsx file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
         <span className='orange_gradient'>OpenAI GPT-4</span>
       </h1>
       <h2 className="desc">
-        Simplify your reading with Summize, an open-source article summarizer that transforms lengthy articles into clear and consice sumarries.
+        Simplify your reading with Sumz, an open-source article summarizer that transforms lengthy articles into clear and consice sumarries.
       </h2>
     </header>
   )
